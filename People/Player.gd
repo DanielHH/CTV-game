dc399324ee029207e38bc4ac6c580788457790f8
Daniel Herzegh <danhe178@student.liu.se>
2018-09-20 18:39:55 +0200

    Restarted some work.

--- People/Player.gd
+++ People/Player.gd
@@ -1,5 +1,20 @@
-extends "res://People/Person.gd"
+extends Area2D
 
+signal hit
+signal dead
+
+export (PackedScene) var Bullet
+export (int) var speed
+export (int) var health
+
+var screensize
+
+
+func _ready():
+	screensize = get_viewport_rect().size
+	hide()
+
+# Controls
 func _process(delta):
 	var velocity = Vector2() # The player's movement vector.
 	if Input.is_action_pressed("ui_right"):
@@ -27,3 +42,15 @@ func _process(delta):
 	elif velocity.y != 0:
     	$AnimatedSprite.animation = "up"
     	$AnimatedSprite.flip_v = velocity.y > 0
+	
+# Colliding with another body
+func _on_Player_body_entered(body):
+    hide() # Player disappears after being hit.
+    emit_signal("hit")
+    $CollisionShape2D.disabled = true
+
+# Starting a new game
+func start(pos):
+    position = pos
+    show()
+    $CollisionShape2D.disabled = false
