3c410c786608ef29314b50e631e0a1b77d12f2a5
Daniel Herzegh <danhe178@student.liu.se>
2019-08-10 15:35:26 +0200

    combine separate scenes for NPCs. Add visuals and behaviour for infected NPC

--- People/NPC.gd
+++ People/NPC.gd
@@ -1,17 +1,22 @@
 extends KinematicBody2D
 
+onready var parent = get_parent()
+
 signal hit
 signal dead
 
 export (int) var speed
 export (int) var health
 
+export (bool) var infected
+
 var velocity = Vector2()
 var alive = true
 
 func _ready():
-	#hide()
-	pass
+	if not infected:
+		$InfectedSmoke.set_visible(false)
+		speed = 100
 
 # Controls
 func _process(delta):
@@ -20,8 +25,10 @@ func _process(delta):
 		set_z_index(0)
 
 func control(delta):
-	pass
-		
+	if parent is PathFollow2D and health > 0:
+		parent.set_offset(parent.get_offset() + speed * delta)
+		position = Vector2()
+	
 func _physics_process(delta):
 	if not alive:
 		return
